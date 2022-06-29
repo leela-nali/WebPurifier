868a4de7cda0156815ed9706c9689bf2ea815ec9
0x7C2f <76737424+0x7C2f@users.noreply.github.com>
2022-06-29 13:32:42 -0400

    added encoding

--- directoryparse.py
+++ directoryparse.py
@@ -1,9 +1,10 @@
 import os
+import urllib.parse
 
 for root, dirs, files in os.walk(r'filters/'):
     for file in files:
         if file.endswith('.txt'):
             output = os.path.join(root, file)
-            encoded = output.replace(" ", "%20")
+            encoded = urllib.parse.quote(output)
             with open('main.txt', 'a') as main:
                 main.write("!#include " + encoded + "\n")
