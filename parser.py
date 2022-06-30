c0dbbcd8948f87366f55ae416e5d105b6f26d7f2
0x7C2f <76737424+0x7C2f@users.noreply.github.com>
2022-06-29 21:17:51 -0400

    updated main.txt

---
+++ parser.py
@@ -0,0 +1,43 @@
+import os
+import urllib.parse
+import json
+
+def main():
+    with open('README.md', 'a') as readme:
+        readme.write("# Whats Included")
+    for root, dirs, files in os.walk(r'filters/'):
+        for file in files:
+            if file.endswith('.txt'):
+                try:
+                    if (isBlacklisted(file) == True):
+                        print("🔴 List is blacklisted")
+                    elif(isWhitelisted(file) == True):
+                        print("✅List is whitelisted")
+                        output = os.path.join(root, file)
+                        encoded = urllib.parse.quote(output)
+                        with open('main.txt', 'a') as main:
+                            main.write("\n!#include " + encoded)
+                        with open('README.md', 'a') as readme:
+                            readme.write("\n- "+file)
+                    else:
+                        print("No data on the list ¯\_(ツ)_/¯")
+                except:
+                    print("Exception thrown. List validation failed.")
+
+def isWhitelisted(file):
+    with open('json/whitelist.json', 'r') as whitelist:
+        wl_data = json.load(whitelist)
+        if(file in wl_data):
+            return True
+        else:
+            return False
+        whitelist.close()
+def isBlacklisted(file):
+    with open('json/blacklist.json', 'r') as blacklist:
+        bl_data = json.load(blacklist)
+        if(file in bl_data):
+            return True
+        else:
+            return False
+        blacklist.close()
+main()
---
+++ setup.sh
@@ -0,0 +1,11 @@
+git submodule update --recursive --remote
+rm -rf main.txt
+rm -rf README.md
+touch main.txt
+touch README.md
+cat header.txt >> main.txt
+find submodules/ -type f -name "*.txt" -exec cp -t filters/ {} +
+python3 parser.py
+git add .
+git commit -m "updated main.txt"
+git push
