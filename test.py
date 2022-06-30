cae3c8766f11ac6397c34fe5459c86abe0aaec42
0x7C2f <76737424+0x7C2f@users.noreply.github.com>
2022-06-30 14:47:16 -0400

    updated main.txt

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
 import sqlite3
 
+database = 'database/filters.db'
+connection = sqlite3.connect(database)
+cur = connection.cursor()
+read = "SELECT filter_name FROM whitelist;"
+cur.execute(read)
+filters = cur.fetchall()
+print(filters)
+connection.close()
