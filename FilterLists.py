1149a396dac4888741f3651fc23818c3dac48d7d
0x7C2f <76737424+0x7C2f@users.noreply.github.com>
2022-07-02 20:45:22 -0400

    updated main.txt

--- FilterLists.py
+++ FilterLists.py
@@ -17,7 +17,7 @@ class FilterLists:
         for list in response_info:
             list_name = list['name']
             syntax_id_unformated = list['syntaxIds']
-            syntax_id=(','.join(lst))
+            syntax_id=(','.join(syntax_id_unformated))
             print(list_name)
             print(syntax_id)
             if(list['syntaxIds'] in whitelist):
