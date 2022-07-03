b1c767f6019a348ef5f475fb00dca7c4e379b053
0x7C2f <76737424+0x7C2f@users.noreply.github.com>
2022-07-02 20:45:08 -0400

    updated main.txt

--- FilterLists.py
+++ FilterLists.py
@@ -16,7 +16,8 @@ class FilterLists:
         response_info = requests.get(url + '/lists').json()
         for list in response_info:
             list_name = list['name']
-            syntax_id = list['syntaxIds']
+            syntax_id_unformated = list['syntaxIds']
+            syntax_id=(','.join(lst))
             print(list_name)
             print(syntax_id)
             if(list['syntaxIds'] in whitelist):
