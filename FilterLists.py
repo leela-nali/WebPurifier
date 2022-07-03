2ae4162b41e6c92c6ea6afeb8eec89216bc1d2e2
0x7C2f <76737424+0x7C2f@users.noreply.github.com>
2022-07-02 20:42:52 -0400

    updated main.txt

--- FilterLists.py
+++ FilterLists.py
@@ -15,8 +15,8 @@ class FilterLists:
     def getLists(self):
         response_info = requests.get(url + '/lists').json()
         for list in response_info:
-            for name in list['name']:
-                print(name)
+            name = list['name']
+            print(name)
             if(list['syntaxIds'] in whitelist):
                 if(list['syntaxIds'] not in blacklist):
                     if (list['primaryViewUrl'].endswith('.txt')):
